727ed53e76e233146ddd5562ebe2bd7dc44709c2
minugem <closehaan@gmail.com>
2023-06-06 03:10:07 +1200

    Updated Website

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function navigation() {
       x.style.display = "block";
     }
   }
+
+// function hero() {
+//   var hero = document.getElementById('hero');
+//   var scrollPosition = window.scrollY;
+//   console.log("hi");
+//   if (scrollPosition === 0) {
+//     hero.style.display = 'block';
+//   } else {
+//     hero.style.display = 'none';
+//   }
+// }
